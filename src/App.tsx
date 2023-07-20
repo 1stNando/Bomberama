863f100d211ab806c1f625164479327ce70a16cb
Fernando Aguilar <sparkmanperspective@gmail.com>
2023-07-19 21:37:54 -0400

    newGame function to FETCH POST

--- src/App.tsx
+++ src/App.tsx
@@ -18,16 +18,35 @@ export function App() {
     mines: 10,
   })
 
+  async function newEasyGame() {
+    //check
+    console.log('Make a new easy game')
+
+    const gameOptions = {
+      difficulty: 0,
+    }
+
+    const url = 'https://minesweeper-api.herokuapp.com/games'
+
+    const fetchOptions = {
+      method: 'POST',
+      header: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(gameOptions),
+    }
+
+    const response = await fetch(url, fetchOptions)
+  }
+
   return (
     <main>
       <h1>Bomberama</h1>
       <h2>
-        <button>Easy Game</button>
+        <button onClick={newEasyGame}>Easy Game</button>
         <button>Intermediate Game</button>
         <button>Hard Game</button>
       </h2>
-      <h3>Mines: 100</h3>
-      <h3>Game #: 200</h3>
+      <h3>Mines: {game.mines}</h3>
+      <h3>Game #: {game.id}</h3>
       <h3></h3>
 
       <ul className="difficulty-0">
